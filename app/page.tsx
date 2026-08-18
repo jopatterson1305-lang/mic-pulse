a07644ff7336711247bbf99d74b4657882ff8443
jopatterson1305-lang <jopatterson1305@gmail.com>
2026-08-18 18:04:34 +0300

    Assemble MIC Pulse premium homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { Hero } from "@/components/hero/Hero";
 import { Navigation } from "@/components/sections/Navigation";
 import { IntelligenceSection } from "@/components/sections/IntelligenceSection";
+import { BusinessSection } from "@/components/sections/BusinessSection";
+import { MarketPulse } from "@/components/sections/MarketPulse";
+import { OpportunitiesSection } from "@/components/sections/OpportunitiesSection";
+import { FounderSection } from "@/components/sections/FounderSection";
+import { FounderServices } from "@/components/sections/FounderServices";
+import { Newsletter } from "@/components/sections/Newsletter";
+import { Footer } from "@/components/sections/Footer";
 
 export default function Home() {
-  return (
-    <main>
-      <Navigation />
-      <Hero />
-      <IntelligenceSection />
-    </main>
-  );
+  return <main><Navigation /><Hero /><IntelligenceSection /><BusinessSection /><MarketPulse /><OpportunitiesSection /><FounderSection /><FounderServices /><Newsletter /><Footer /></main>;
 }
